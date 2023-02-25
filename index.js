f29b1a9e27be981491f6bc52b59b943f1f2f4abf
DIPANKAR <you@example.com>
2023-02-25 18:12:12 +0600

    code added for pagination

--- index.js
+++ index.js
@@ -17,10 +17,30 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tmkhisl.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+        app.get('/products', async (req, res) => {
+            const page = req.query.page;
+            const size = req.query.size;
+            console.log(page, size);
 
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.log(err));
 
 
 app.listen(port, () => {
